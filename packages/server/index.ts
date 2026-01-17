a7872b84477aeed9e56f788d12774452546df367
Shaz Gill <shazgill@Mac.lan>
2026-01-18 00:24:25 +0800

    Setting up Zod for validation and maintaining conversations by using new Map()

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,7 +1,8 @@
-import express from 'express';
-import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import type { Request, Response } from 'express';
+import express from 'express';
 import OpenAI from 'openai';
+import z from 'zod';
 
 dotenv.config();
 
@@ -19,15 +20,39 @@ app.get('/api/hello', (req: Request, res: Response) => {
    });
 });
 
+// conversationId -> lastResponseId
+// conv1 -> 100
+// conv1 -> 200
+const conversations = new Map<string, string>();
+
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'Prompt is required.')
+      .max(1000, 'Prompt is too long (max 1000 characters).'),
+   conversationId: z.string().uuid(),
+});
+
 app.post('/api/chat', async (req: Request, res: Response) => {
-   const { prompt } = req.body;
+   const parseResult = chatSchema.safeParse(req.body);
+
+   if (!parseResult.success) {
+      res.status(400).json(parseResult.error.format());
+      return;
+   }
+
+   const { prompt, conversationId } = req.body;
 
    const response = await client.responses.create({
       model: 'gpt-4o-mini',
       input: prompt,
       temperature: 0.2, // This is actually for creative responses
       max_output_tokens: 100,
+      previous_response_id: conversations.get(conversationId),
    });
+
+   conversations.set(conversationId, response.id);
 
    res.json({ message: response.output_text });
 });
