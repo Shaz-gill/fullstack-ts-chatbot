893deeb02b9d67bde6fd7b21d289f4cf561ab7c4
Shaz Gill <shazgill@Mac.lan>
2026-01-19 22:20:50 +0800

    Making changes in ChatBot.tsx component

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -1,10 +1,31 @@
 import { Button } from './ui/button';
 import { FaArrowUp } from 'react-icons/fa';
+import { useForm } from 'react-hook-form';
+
+type FormData = {
+   prompt: string;
+};
 
 export const ChatBot = () => {
+   const { register, handleSubmit, reset } = useForm<FormData>();
+
+   const onSubmit = (data: FormData) => {
+      reset();
+   };
+
    return (
-      <div className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl">
+      <form
+         onSubmit={handleSubmit(onSubmit)}
+         onKeyDown={(e) => {
+            if (e.key === 'Enter' && !e.shiftKey) {
+               e.preventDefault();
+               handleSubmit(onSubmit)();
+            }
+         }}
+         className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
+      >
          <textarea
+            {...register('prompt', { required: true })}
             className="w-full border-0 focus:outline-0 resize-none"
             placeholder="Ask anything"
             maxLength={1000}
@@ -12,6 +33,6 @@ export const ChatBot = () => {
          <Button className="rounded-full w-9 h-9">
             <FaArrowUp />
          </Button>
-      </div>
+      </form>
    );
 };
