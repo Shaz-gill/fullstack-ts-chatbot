0f9cf8e1c446f5665aba8a435a5d8079bcc17b24
Shaz Gill <shazgill@Mac.lan>
2026-01-19 23:11:00 +0800

    Form submission and validation in Chatbot.tsx component

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -1,36 +1,44 @@
-import { Button } from './ui/button';
-import { FaArrowUp } from 'react-icons/fa';
 import { useForm } from 'react-hook-form';
+import { FaArrowUp } from 'react-icons/fa';
+import { Button } from './ui/button';
 
 type FormData = {
    prompt: string;
 };
 
 export const ChatBot = () => {
-   const { register, handleSubmit, reset } = useForm<FormData>();
+   const { register, handleSubmit, reset, formState } = useForm<FormData>();
 
    const onSubmit = (data: FormData) => {
+      console.log(data);
       reset();
    };
 
+   const onKeyDown = (e: React.KeyboardEvent<HTMLFormElement>) => {
+      if (e.key === 'Enter' && !e.shiftKey) {
+         e.preventDefault();
+         submit(); // 'submit' returns a function named 'handleSubmit', so, we have to explicitly call that function.
+      }
+   };
+
+   const submit = handleSubmit(onSubmit);
+
    return (
       <form
-         onSubmit={handleSubmit(onSubmit)}
-         onKeyDown={(e) => {
-            if (e.key === 'Enter' && !e.shiftKey) {
-               e.preventDefault();
-               handleSubmit(onSubmit)();
-            }
-         }}
+         onSubmit={submit} // returns a function
+         onKeyDown={onKeyDown}
          className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
       >
          <textarea
-            {...register('prompt', { required: true })}
+            {...register('prompt', {
+               required: true,
+               validate: (data) => data.trim().length > 0,
+            })}
             className="w-full border-0 focus:outline-0 resize-none"
             placeholder="Ask anything"
             maxLength={1000}
          />
-         <Button className="rounded-full w-9 h-9">
+         <Button disabled={!formState.isValid} className="rounded-full w-9 h-9">
             <FaArrowUp />
          </Button>
       </form>
