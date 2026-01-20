4c7a4d3143c3706b7b21f886290d0b573d6450cf
Shaz Gill <shazgill@Mac.lan>
2026-01-20 21:00:04 +0800

    Styling Chatbot component

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useRef, type KeyboardEvent } from 'react';
+import { useRef, useState, type KeyboardEvent } from 'react';
 import { useForm } from 'react-hook-form';
 import { FaArrowUp } from 'react-icons/fa';
 import { Button } from './ui/button';
@@ -8,21 +8,46 @@ type FormData = {
    prompt: string;
 };
 
+type ChatResponse = {
+   message: string;
+};
+
+type Message = {
+   content: string;
+   role: 'user' | 'bot';
+};
+
 export const ChatBot = () => {
+   const [messages, setMessage] = useState<Message[]>([]);
+
    // We used 'ref hook' because 'conversationId' should be created once and should not change and re-render, that is the difference between the ref and state hooks.
    // With 'ref hook', we can store values that should not cause a re-render. It's good for timers, IDs.
    const conversationId = useRef(crypto.randomUUID());
    const { register, handleSubmit, reset, formState } = useForm<FormData>();
 
    const onSubmit = async ({ prompt }: FormData) => {
+      setMessage((prev) => [
+         ...prev,
+         {
+            content: prompt,
+            role: 'user',
+         },
+      ]);
+
       reset();
 
-      const { data } = await axios.post('/api/chat', {
+      const { data } = await axios.post<ChatResponse>('/api/chat', {
          prompt,
          conversationId: conversationId.current,
       });
 
-      console.log('Data:', data);
+      setMessage((prev) => [
+         ...prev,
+         {
+            content: data.message,
+            role: 'bot',
+         },
+      ]);
    };
 
    const onKeyDown = (e: KeyboardEvent<HTMLFormElement>) => {
@@ -35,23 +60,42 @@ export const ChatBot = () => {
    const submit = handleSubmit(onSubmit);
 
    return (
-      <form
-         onSubmit={submit} // returns a function
-         onKeyDown={onKeyDown}
-         className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
-      >
-         <textarea
-            {...register('prompt', {
-               required: true,
-               validate: (data) => data.trim().length > 0,
-            })}
-            className="w-full border-0 focus:outline-0 resize-none"
-            placeholder="Ask anything"
-            maxLength={1000}
-         />
-         <Button disabled={!formState.isValid} className="rounded-full w-9 h-9">
-            <FaArrowUp />
-         </Button>
-      </form>
+      <div>
+         <div className="flex flex-col gap-3 mb-10">
+            {messages.map((message, index) => (
+               <p
+                  key={index}
+                  className={`px-3 py-1 rounded-xl ${
+                     message.role === 'user'
+                        ? 'bg-blue-600 text-white self-end'
+                        : 'bg-gray-100 text-black self-start'
+                  }`}
+               >
+                  {message.content}
+               </p>
+            ))}
+         </div>
+         <form
+            onSubmit={submit} // returns a function
+            onKeyDown={onKeyDown}
+            className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl"
+         >
+            <textarea
+               {...register('prompt', {
+                  required: true,
+                  validate: (data) => data.trim().length > 0,
+               })}
+               className="w-full border-0 focus:outline-0 resize-none"
+               placeholder="Ask anything"
+               maxLength={1000}
+            />
+            <Button
+               disabled={!formState.isValid}
+               className="rounded-full w-9 h-9"
+            >
+               <FaArrowUp />
+            </Button>
+         </form>
+      </div>
    );
 };
